62789583270522cbbf8c291b5fb7432caf3697e8
Suhas44 <suhas8@outlook.com>
2022-07-27 22:50:06 -0500

    Added Commands to Right-Click Menu

--- extension.js
+++ extension.js
@@ -2,27 +2,41 @@ const vscode = require('vscode');
 const editor = vscode.window.activeTextEditor;
 
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('escapesquared.escapesquared', async function () {
-		let text = await vscode.env.clipboard.readText()
-		vscode.window.showQuickPick(["Escape Copied Text", "Escape Highlighted Text"], {}).then(function (selection) {
-			if (selection === "Escape Copied Text") {escapeCopiedText(text)} else if (selection == "Escape Highlighted Text") {escapeHighlightedText()}
+	let palette = vscode.commands.registerCommand('escapesquared.escapesquared', async function () {
+		let picks = ["Escape Copied Text", "Escape Highlighted Text"];
+		vscode.window.showQuickPick(picks, {}).then(function(s) {
+			if (s == picks[0]) {escapeCopiedText()} else if (s == picks[1]) {escapeHighlightedText()}
 		});
 	});
-	context.subscriptions.push(disposable);
+
+	let copiedText = vscode.commands.registerCommand('escapesquared.copiedtext', function () {
+		escapeCopiedText();
+	});
+
+	let highlightedText = vscode.commands.registerCommand('escapesquared.highlightedtext', function () {
+		escapeHighlightedText();
+	});
+
+	context.subscriptions.push(palette, copiedText, highlightedText);
 }
 
-function escapeCopiedText(text) {
-	if (editor) {
-		editor.edit(editBuilder => {
-			editBuilder.insert(editor.selection.active, escape(text));
-		});
-	}
+async function escapeCopiedText() {
+	/* Probably not going to include this in the extension
+	const selection = await vscode.window.showQuickPick(['zero', 'one', 'two', 'three', 'four', 'five'], {
+		canPickMany: true
+	});
+	Object.keys(selection).map(key => {
+		vscode.window.showInformationMessage(selection[key]);
+	}); */
+	let text = await vscode.env.clipboard.readText();
+	editor.edit(editBuilder => {
+		editBuilder.insert(editor.selection.active, escape(text));
+	});
 }
 function escapeHighlightedText() {
 	const selection = editor.selection;
 	if (selection && !selection.isEmpty) {
-		const firstSelectedCharacter = selection.start.character;
-		const selectionRange = new vscode.Range(selection.start.line, firstSelectedCharacter, selection.end.line, selection.end.character);
+		const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
 		const highlighted = editor.document.getText(selectionRange);
 		editor.edit(builder => {
 			builder.replace(selection, escape(highlighted));
@@ -32,10 +46,23 @@ function escapeHighlightedText() {
 
 function escape(text) {
 	text = text.replace(/\\/g, '\\\\');
-	/*
-	if ((text[0] != '"' && text[text.length - 1] != '"') && (text[0] != '\''&& text[text.length - 1] != '\'')) {
-		text = `"${text}"` // should be an option in settings to disable automatic quotes
-	} */
+	// Clean Up This Logic
+	if (text[0] == '"' && text[text.length - 1] == '"') {
+		text = text.slice(1, -1);
+		text = text.replace(/"/g, '\\"');
+		text = text.replace(/'/g, "\\'");
+		text = `"${text}"`; // should be an option in settings to disable automatic quotes
+		return text;
+	} else if (text[0] == '\''&& text[text.length - 1] == '\'') {
+		text = text.slice(1, -1);
+		text = text.replace(/"/g, '\\"');
+		text = text.replace(/'/g, "\\'");
+		text = `'${text}'`;
+		return text;
+	}
+	text = text.replace(/"/g, '\\"');
+	text = text.replace(/'/g, "\\'");
+	text = `"${text}"`;
 	return text;
 }
 
